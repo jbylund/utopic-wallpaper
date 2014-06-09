c25e29e14d7a0119809321c9723ac05ad2f8344b
Joseph Bylund <joseph.bylund@gmail.com>
2014-06-09 00:38:07 -0400

    (joe) serving all assets over https in prep of going ssl

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <title>Ubuntu 14.10 (Utopic Unicorn) Wallpaper Contest</title>
 <!-- Main style sheets for CSS2 capable browsers -->
 
-<link rel="stylesheet" type="text/css" media="screen" href="http://assets.ubuntu.com/sites/guidelines/css/dev/beta/ubuntu-styles.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="http://assets.ubuntu.com/sites/ubuntu/1072/u/css/styles.css" />
-<link rel="stylesheet" type="text/css" media="screen and (min-width: 768px)" href="http://assets.ubuntu.com/sites/ubuntu/latest/u/css/global.css" />
-<link rel="stylesheet" type="text/css" media="print" href="http://assets.ubuntu.com/sites/ubuntu/1072/u/css/core-print.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="https://assets.ubuntu.com/sites/guidelines/css/dev/beta/ubuntu-styles.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="https://assets.ubuntu.com/sites/ubuntu/1072/u/css/styles.css" />
+<link rel="stylesheet" type="text/css" media="screen and (min-width: 768px)" href="https://assets.ubuntu.com/sites/ubuntu/latest/u/css/global.css" />
+<link rel="stylesheet" type="text/css" media="print" href="https://assets.ubuntu.com/sites/ubuntu/1072/u/css/core-print.css" />
 
 
 </head>
@@ -22,13 +22,13 @@ function draw_login_button()
   if (isset($_SESSION['logged']) && $_SESSION['logged'] )
   {
     echo '<a href="logout.php" title="Logout">
-    <img src="http://assets.ubuntu.com/sites/ubuntu/latest/u/img/logos/logo-ubuntu-orange.svg">
+    <img src="https://assets.ubuntu.com/sites/ubuntu/latest/u/img/logos/logo-ubuntu-orange.svg">
     </a>';
   }
   else
   {
     echo '<a href="try_auth.php" title="Login">
-    <img src="http://assets.ubuntu.com/sites/ubuntu/latest/u/img/logos/logo-ubuntu-orange.svg">
+    <img src="https://assets.ubuntu.com/sites/ubuntu/latest/u/img/logos/logo-ubuntu-orange.svg">
     </a>';
   }
 }
