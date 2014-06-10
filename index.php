b6b72c35d3f90e56cadf8c201043c0aa9bc4031a
Joseph Bylund <joseph.bylund@gmail.com>
2014-06-10 00:22:26 -0400

    (joe) including jquery and the slimbox code

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 <link rel="stylesheet" type="text/css" media="screen" href="https://assets.ubuntu.com/sites/ubuntu/latest/u/css/styles.css" />
 <link rel="stylesheet" type="text/css" media="screen and (min-width: 768px)" href="https://assets.ubuntu.com/sites/ubuntu/latest/u/css/global.css" />
 <link rel="stylesheet" type="text/css" media="print" href="https://assets.ubuntu.com/sites/ubuntu/latest/u/css/core-print.css" />
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="src/slimbox2.js"></script>
+<script type="text/javascript" src="src/autoload.js"></script>
+<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" />
 
 </head>
 <body>
