51d0892ec97cc192e29410da922c281ed6793116
Joseph Bylund <joseph.bylund@gmail.com>
2014-06-12 00:23:08 -0400

    (joe) moving make title to the base dir...

---
+++ make_title.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import json
+
+def name_to_number(iname):
+  index = ord(iname[0].lower()) - ord('a') + 1
+  year = index // 2 + 4
+  month = 4 + 6 * (index % 2)
+  return "{}.{}".format(year, str(month).rjust(2,"0"))
+
+config = json.load(open("./config.json"))
+release = sys.argv[1].split(".")[0]
+if release in config:
+  print "Ubuntu {} {} ({}) Wallpapers".format(release, config[release]['animal'], name_to_number(release)).title()
+else:
+  print "?"
