f1cfd5d6949cbd53e7794accb8d5cfaf598065aa
Joseph Bylund <joseph.bylund@gmail.com>
2014-06-12 00:19:07 -0400

    (joe) dynamically generated page titles and starting to apply the css layout and put things in their proper places.  This is pretty exciting because I think it might start to come together.

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-<title>Ubuntu 14.10 (Utopic Unicorn) Wallpaper Contest</title>
+<title>
+<?php
+echo passthru("/usr/bin/python make_title.py ".$_SERVER['HTTP_HOST']);
+?>
+</title>
 <!-- Main style sheets for CSS2 capable browsers -->
-
 <link rel="stylesheet" type="text/css" media="screen" href="https://assets.ubuntu.com/sites/guidelines/css/dev/beta/ubuntu-styles.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="https://assets.ubuntu.com/sites/ubuntu/latest/u/css/styles.css" />
 <link rel="stylesheet" type="text/css" media="screen and (min-width: 768px)" href="https://assets.ubuntu.com/sites/ubuntu/latest/u/css/global.css" />
 <link rel="stylesheet" type="text/css" media="print" href="https://assets.ubuntu.com/sites/ubuntu/latest/u/css/core-print.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="local.css" />
 <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
 <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="src/slimbox2.js"></script>
@@ -16,6 +20,13 @@
 
 </head>
 <body>
+<div id="headerwrapper">
+  <div id="header">
+    <div>
+      <?php echo passthru("python make_title.py ".$_SERVER['HTTP_HOST']); ?>
+    </div>
+  </div>
+</div>
 <div id="pageWrapper">
 <?php
 require_once "common.php";
@@ -48,9 +59,31 @@ function draw_login_button()
   }
 }
 
-draw_login_button();
+draw_login_button(); 
 echo passthru("./filter_photos.py ".$_SERVER['HTTP_HOST']." ".$_SESSION['seed'])
 ?>
 </div>
 </body>
 </html>
+
+
+
+<?php
+#  <div id="wrapper">
+#    <div id="leftcontainer">
+#      <div id="leftcontents">
+#        CONTENTS
+#      </div>
+#    </div>
+#    <div id="rightcontainer">
+#      <div id="rightcontents">
+#        <div id="rightinner">
+#          CONTENTS
+#        </div>
+#      </div>
+#    </div>
+#  </div>
+#  <div id="footer">
+#    Footer
+#  </div>
+?>
