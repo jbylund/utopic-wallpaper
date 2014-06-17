2f4483236891cd00a8b320394261b9cbfded8973
Joseph Bylund <joseph.bylund@gmail.com>
2014-06-16 23:43:34 -0400

    (joe) classing the drop targets

--- make_drop_targets.py
+++ make_drop_targets.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # make drop targets for ubuntu voting
 for i in xrange(10):
-  print '<div class="droptarget">{}</div>'.format(i+1)
+  print '<div class="droptarget dropsize">{}</div>'.format(i+1)
